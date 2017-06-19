4943d7faf5d4d5f6916524c43f8921b27e0f4847
Jonas Wirth <jonas-wirth@gmx.ch>
2017-06-19 18:31:54 +0200

    Initial commit

---
+++ Api/Web/src/app.ts
@@ -0,0 +1,18 @@
+﻿declare var require: any;
+
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+import Start from './components/start'
+
+@Component({
+    name: 'app',
+    template: require('./app.html'),
+    components: {
+        Start
+    }
+})
+
+export default class App extends Vue {
+    message: string;
+}
---
+++ Api/Web/src/components/start.ts
@@ -0,0 +1,13 @@
+﻿declare var require: any;
+
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+@Component({
+    name: 'start',
+    template: require('./start.html')
+})
+
+export default class Start extends Vue {
+
+}
